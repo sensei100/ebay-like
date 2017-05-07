b3f0924c9a12a1aedeaf41de59ff8f9f41af7bfb
sensei100 <jenanton01@gmail.com>
2017-05-07 14:02:14 -0400

    set up item model validations and enum

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  validates :title, :description, :price, :category_id, :brand_id, :condition, :format, :screen_size, :color, :memory, :presence: true
+
+  enum category_id: [:notebook, :ultrabook, :netbook]
+  enum brand_id: [:apple, :lenovo, :dell, :hp, :asus]
+  enum color: [:white, :black, :silver, :gold]
+  enum format: [:buy_now, :auction]
+  enum condition: [:brand_new, :used, :refurbished]
+
+  MEMORY_SIZES =  2.upto(8).each_with_object({}) { |item, hash| hash[item] = "#{item}GB" }
+  SCREEN_SIZES = %w(11.6 13.3 14 15.4).each_with_object({}) { |item, hash| hash[item] = "#{item} inches" }
 end
