7acceaae7a70b9ab017ce34c01ddd349f2ee7e0a
sensei100 <jenanton01@gmail.com>
2017-05-07 14:56:46 -0400

    update item class with solr callbacks

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,23 @@ class Item < ApplicationRecord
 
   MEMORY_SIZES =  2.upto(8).each_with_object({}) { |item, hash| hash[item] = "#{item}GB" }
   SCREEN_SIZES = %w(11.6 13.3 14 15.4).each_with_object({}) { |item, hash| hash[item] = "#{item} inches" }
+
+  after_commit :index_data_in_solr, on: [:create, :update]
+  before_destroy :remove_data_from_solr
+
+  def to_solr
+    {
+      "title" => title, "description" => description, "price" => price, "category" => category_id.titleize, "brand" => brand_id.titleize, "condition" => condition.titleize, "format" => format.titleize, "screen_size" => screen_size, "color" => color.titleize, "memory" => memory, "id" => id
+    }
+  end
+
+  def index_data_in_solr
+    SolrService.add(to_solr)
+    SolrService.commit 
+  end
+
+  def remove_data_from_solr
+    SolrService.delete_by_id(id)
+    SolrService.commit
+  end
 end
